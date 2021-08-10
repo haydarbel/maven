059f663308f167042625a3edafaeabfb3c4eedcf
haydar koktas <haydarkoktas@gmail.com>
2021-08-10 15:14:27 +0200

    fault

--- src/test/java/be/vdab/goededoel/domain/GoedeDoelTest.java
+++ src/test/java/be/vdab/goededoel/domain/GoedeDoelTest.java
@@ -6,19 +6,23 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
+
 class GoedeDoelTest {
-	private static final String NAAM = "CliniClowns";
-	private GoedeDoel doel;
+    private static final String NAAM = "CliniClowns";
+    private GoedeDoel doel;
 
-	@BeforeEach
-	void beforeEach() {
-		doel = new GoedeDoel(NAAM);
-	}
+    @BeforeEach
+    void beforeEach() {
+        doel = new GoedeDoel(NAAM);
+    }
 
-	@Test
-	void getNaam() {
-		assertThat(doel.getNaam()).isEqualTo(NAAM);
-	}
+    @Test
+    void getNaam() {
+        assertThat(doel.getNaam()).isEqualTo(NAAM);
+    }
 
-	
+    @Test
+    void eenNieuwDoelHeeftNogGeenOpbrengst() {
+        assertThat(doel.getOpbrengst()).isZero();
+    }
 }
